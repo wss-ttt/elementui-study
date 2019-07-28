4989d23e8e18ffae34c6519de011953370470971
王水胜 <602466011@qq.com>
2019-07-28 16:04:39 +0800

    更新：给菜单学习下的index4添加展开折叠功能

--- src/views/菜单学习/index4.vue
+++ src/views/菜单学习/index4.vue
@@ -1,19 +1,24 @@
 <template>
 	<div class="wrapper">
 		<div class="sidebar">
-			<el-menu 
+			<el-menu class="sidebar-el-menu"
 			background-color="#545c64" 
 			text-color="#fff" 
 			active-text-color="#ffd04b"
-			default-active="home">
+			default-active="home" :collapse="isCollapse">
 				<!-- <el-menu-item index="1"><i class="el-icon-menu"></i>首页</el-menu-item>
 				<el-menu-item index="2"><i class="el-icon-document"></i>相册</el-menu-item>
 				<el-menu-item index="3"><i class="el-icon-setting"></i>博客</el-menu-item>
 				<el-menu-item index="4"><i class="el-icon-location"></i>新闻</el-menu-item> -->
-				
+				<el-menu-item>
+					<!--展开-->
+					<i class="el-icon-s-unfold" v-if="isCollapse" @click="isCollapse=false"></i>
+					<!--折叠-->
+					<i class="el-icon-s-fold" v-else @click="isCollapse=true"></i>
+				</el-menu-item>
 				<el-menu-item v-for="(item,index) in items" :index="item.index" :key="item.index">
 					<i :class="item.icon"></i>
-					{{item.title}}
+					<span>{{item.title}}</span>
 				</el-menu-item>
 			</el-menu>
 		</div>
@@ -24,6 +29,7 @@
 		name:'',
 		data(){
 			return{
+				isCollapse: true, // 是否折叠菜单
 				items:[
 					{
 						icon:'el-icon-menu',
@@ -52,11 +58,14 @@
 </script>
 <style lang="scss" scoped="scoped">
 	.sidebar{
-		width: 250px;
+		/*width: 250px;*/
 		position: absolute;
 		top:0;
 		left: 0;
 		bottom: 0;
 		border: 1px solid #ddd;
+	}
+	.sidebar-el-menu:not(.el-menu--collapse) {
+		width: 250px;
 	}
 </style>
