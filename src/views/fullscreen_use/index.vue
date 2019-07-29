e40cd5dfacb9594f80bb383132a1a4cf692d9dd1
王水胜 <602466011@qq.com>
2019-07-29 11:04:23 +0800

    更新：安装screenfull 依赖 (npm install screenfull --save 为生产依赖)

--- src/views/fullscreen_use/index.vue
+++ src/views/fullscreen_use/index.vue
@@ -1,24 +1,42 @@
 <template>
 	<div class="wrapper">
 		<el-tooltip effect="dark" placement="bottom" :content="fullscreen?'取消全屏':'全屏'">
-			<i class="el-icon-rank v-icon"></i>
+			<i class="el-icon-rank v-icon" @click="handleFullscreen"></i>
 		</el-tooltip>
 	</div>
 </template>
 
 <script>
-	export default{
-		name:'',
-		data(){
-			return{
-				fullscreen:false,
+	import screenfull from 'screenfull'
+	export default {
+		name: '',
+		data() {
+			return {
+				fullscreen: false,
+			}
+		},
+		methods: {
+			handleFullscreen() {
+				if(!screenfull.enabled) {
+					this.$message({
+						message: '不支持全屏',
+						type: 'warning'
+					})
+					return false
+				}
+				screenfull.toggle();
+				this.fullscreen = !this.fullscreen;
+				this.$message({
+					type:'success',
+					message:'全屏'
+				});
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped="scoped">
-	.v-icon{
-		font-size:30px;
+	.v-icon {
+		font-size: 30px;
 	}
 </style>
