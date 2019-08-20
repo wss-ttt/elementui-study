d0e6edba902c4d328713860a70fd7275c9b0848a
王水胜 <602466011@qq.com>
2019-08-20 21:31:36 +0800

    更新：更新版本

---
+++ src/views/test2/005-messagecenter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <el-button @click="test">message</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+        documentClientHeight:'',
+    };
+  },
+  watch: {},
+  computed: {
+
+  },
+  methods: {
+    test() {
+      this.$message({
+        message: "给我一首歌的时间",
+        type: "error",
+        duration:0,
+        offset: this.documentClientHeight/2
+      });
+    },
+  },
+  created() {},
+  mounted() {
+      this.documentClientHeight = document.documentElement["clientHeight"];
+      window.onresize = () => {
+        this.documentClientHeight = document.documentElement["clientHeight"];
+      };
+  }
+};
+</script>
+<style scoped>
+.wrapper {
+}
+</style>
