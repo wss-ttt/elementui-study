c85ff62244861e08633d0ea5895e7ec789bd7a1a
王水胜 <602466011@qq.com>
2019-08-22 10:53:42 +0800

    更新：更改类型

--- src/views/菜单学习/components/SidebarItem.vue
+++ src/views/菜单学习/components/SidebarItem.vue
@@ -1,13 +1,13 @@
 <template>
   <div v-if="item.children">
     <template v-if="item.children.length == 0">
-        <el-menu-item :index="item.menuId">
+        <el-menu-item :index="item.menuId+''">
           <i class="el-icon-menu"></i>
           {{item.menuName}}
         </el-menu-item>
     </template>
 
-    <el-submenu v-else :index="item.menuId">
+    <el-submenu v-else :index="item.menuId+''">
       <template slot="title" >
         <i class="el-icon-menu"></i>
         {{item.menuName}}
@@ -18,7 +18,7 @@
           v-if="child.children&&child.children.length>0"
           :item="child"
           :key="child.menuId"/>
-        <el-menu-item v-else :key="child.menuId" :index="child.menuId">
+        <el-menu-item v-else :key="child.menuId" :index="child.menuId+''">
           <i class="el-icon-location"></i>
           {{child.menuName}}
         </el-menu-item>
