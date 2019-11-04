78295b783ba8c9abb61733bf366560da0f28f439
王水胜 <602466011@qq.com>
2019-11-04 17:37:25 +0800

    添加:添加文件

---
+++ src/views/test2/011-computed.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+      {{userName}}
+      <br>
+      <button @click="change">改变</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+        name:'黑色毛衣'
+    }
+  },
+  computed: {
+    userName: {
+      get() {
+        return this.name
+      },
+      set(val){
+          this.name = val
+      }
+    }
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  destroyed() {},
+  watch: {},
+  methods: {
+      change(){
+          this.name = '给我一首歌的时间'
+      }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/test2/012-clientHeight.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <div class="header">header</div>
+    <div class="content" :style="{height:contentHeight+'px'}">content</div>
+  </div>
+</template>
+
+<script>
+// study:让content的高度为可视区域的高度(会变化的) - header的高度(为固定高度80px)
+export default {
+  props: {},
+  data() {
+    return {
+      clientHeight: document.documentElement['clientHeight']
+    }
+  },
+  computed: {
+    contentHeight: {
+      get() {
+        return this.clientHeight - 80
+      },
+      set(val) {
+        this.clientHeight = val
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', () => {
+      console.log('可视区域的高度', this.clientHeight)
+      this.clientHeight = document.documentElement['clientHeight']
+    })
+  },
+  watch: {},
+  methods: {}
+}
+</script>
+<style scoped lang="scss">
+.wrapper {
+  height: 100%;
+  .header {
+    height: 80px;
+    background-color: #1acd7e;
+  }
+  .content {
+    background-color: antiquewhite;
+  }
+}
+</style>
