cdcbeecedbcdde3050d774b21b45a521c977d530
王水胜 <602466011@qq.com>
2019-11-04 15:21:18 +0800

    更新：render函数使用测试

---
+++ src/views/test2/010-render3.vue
@@ -0,0 +1,47 @@
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      name: '乔峰'
+    }
+  },
+  methods: {
+    clickHandle() {
+      this.name = '周杰伦'
+    }
+  },
+  render(h) {
+    return h(
+      'h1',
+      {
+        class: {
+          border: true,
+          success: true
+        }
+      },
+      [
+        this.name,
+        h(
+          'button',
+          {
+            on: {
+              click: this.clickHandle
+            }
+          },
+          '改变'
+        )
+      ]
+    )
+  }
+}
+</script>
+<style scoped>
+.border {
+  border: 1px solid red;
+}
+.success {
+  background-color: #67c23a;
+}
+</style>
---
+++ src/views/test2/010-render4.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      name: '乔峰'
+    }
+  },
+  render(h) {
+    return h('h1', this.name)
+  },
+
+  // 上面的代码可以简写为如下:
+  /* render: h => {
+    console.log(this)
+    console.log(this.name)
+    return h('h1', this.name)   // 取不到"乔峰"
+  } */
+}
+</script>
