e92c26c8ff70561862c14425703228b8df1ed006
王水胜 <602466011@qq.com>
2019-11-14 09:23:37 +0800

    更新：使用float + 100%实现自适应布局

--- src/views/test2/016-layout.vue
+++ src/views/test2/016-layout.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrapper">
-    <el-row :gutter="15" class="row">
+    <div class="header">header</div>
+    <!-- <el-row :gutter="15" class="row">
       <el-col :span="8" class="col">
         <div class="box first" >1</div>
       </el-col>
@@ -19,7 +20,27 @@
       <el-col :span="8" class="col">
         <div class="box third">3</div>
       </el-col>
-    </el-row>
+    </el-row>-->
+    <div class="content">
+      <!-- <el-row :gutter="15">
+        <el-col :span="8">
+          <div class="one">1</div>
+        </el-col>
+        <el-col :span="8">
+          <div class="two">2</div>
+        </el-col>
+        <el-col :span="8">
+          <div class="three">3</div>
+        </el-col>
+      </el-row> -->
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+    </div>
+
     <!-- <el-row :gutter="15">
       <el-col :span="8">
         <div class="box first">1</div>
@@ -30,7 +51,7 @@
       <el-col :span="8">
         <div class="box third">3</div>
       </el-col>
-    </el-row> -->
+    </el-row>-->
   </div>
 </template>
 
@@ -49,7 +70,7 @@ export default {
   methods: {}
 }
 </script>
-<style scoped>
+<style scoped lang="scss">
 .wrapper {
   padding: 15px;
   height: 100%;
@@ -65,14 +86,44 @@ export default {
   background-color: blueviolet;
 }
 
-.row{
+.row {
   height: 100%;
 }
-.col{
+.col {
   height: 50%;
-  margin:15px 0;
+  margin: 15px 0;
 }
-.box{
+.box {
   height: 100%;
+}
+.header {
+  height: 100px;
+  background-color: azure;
+}
+
+.content{
+  background-color: cadetblue;
+  height: calc(100% - 100px);
+  padding:15px 0;
+  >div{
+    float: left;
+    width: calc((100% - 15px*2)/3);   // 一行放3个
+    margin-right:15px;
+    margin-bottom:15px;
+    &:nth-child(3n){
+      margin-right:0
+    }
+    height: calc((100% - 15px)/2);
+    background-color: red;
+  }
+}
+.one {
+  background-color: red;
+}
+.two {
+  background-color: aqua;
+}
+.three {
+  background-color: blueviolet;
 }
 </style>
