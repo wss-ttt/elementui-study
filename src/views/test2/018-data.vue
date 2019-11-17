58583ff5e49b90f10a62d3ab43aeaeea9b8ed79d
wangshuisheng <602466011@qq.com>
2019-11-17 21:23:15 +0800

    更新;此时变量写在data里面和最外面的区别

---
+++ src/views/test2/018-data.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="data">
+    <v-box username="张三" :option="option"></v-box>
+    <v-box username="李四" :option="{}"></v-box>
+    <v-box username="王五" :option="option"></v-box>
+  </div>
+</template>
+
+<script>
+import VBox from './components/Box'
+export default {
+  props: {},
+  data() {
+    return {
+      option: {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line'
+          }
+        ]
+      }
+    }
+  },
+  computed: {},
+  components: {
+    VBox
+  },
+  created() {},
+  mounted() {},
+  destroyed() {},
+  watch: {},
+  methods: {}
+}
+</script>
+<style scoped>
+</style>
