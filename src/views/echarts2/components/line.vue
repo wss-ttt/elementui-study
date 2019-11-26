634c800f02d134c58cbff99606fe4093f38d3e0a
wangshuisheng <602466011@qq.com>
2019-11-26 22:37:09 +0800

    添加:添加echarts中的line组件 并且通过合并对象的方式传递数据

---
+++ src/views/echarts2/components/line.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="line-chart" :id="id" :style="{height:height,width:width}"></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import uuidv1 from 'uuid/v1'
+export default {
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    width: {
+      type: String,
+      default: '200px'
+    },
+    height: {
+      type: String,
+      default: '200px'
+    }
+  },
+  data() {
+    return {
+      baseConfig: {
+        title: {
+          text: '折线图'
+        },
+        xAxis: {
+          type: 'category',
+          data: []
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: []
+      },
+      id: ''
+    }
+  },
+  computed: {},
+  components: {},
+  created() {
+    let id = uuidv1()
+    this.id = id
+  },
+  mounted() {
+    this.initChart()
+  },
+  destroyed() {},
+  watch: {
+    data: {
+      handler(newVal, oldVal) {
+        console.log('这个执行了没')
+        this.initChart()
+      },
+      deep: true
+    }
+  },
+  methods: {
+    initChart() {
+      let oBox = document.getElementById(this.id)
+      let myChart = echarts.init(oBox)
+      if (this.isEmptyObject(this.data)) {
+        // 数据为空
+        oBox.removeAttribute('_echarts_instance_')
+        oBox.innerHTML = '暂无数据'
+      } else {
+        // 数据不为空
+        /* let option = this.baseConfig
+        option.xAxis.data = this.data.xAxis.data
+        option.series = this.data.series */
+        // 合并对象
+        let option = Object.assign({},this.baseConfig,this.data)
+        console.log('option',option)
+        myChart.setOption(option)
+        window.addEventListener('resize', () => {
+          myChart.resize()
+        })
+      }
+    },
+    isEmptyObject(obj) {
+      return Object.keys(obj).length === 0
+    }
+  }
+}
+</script>
+<style scoped>
+.line-chart {
+  border: 1px solid #ccc;
+}
+</style>
