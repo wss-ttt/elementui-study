a5bf2e1236180cb9d651c5fe8c90e8723e6260b4
王水胜 <602466011@qq.com>
2020-05-06 13:56:31 +0800

    更新:main.js中全局引入swiper

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import ElementUI from 'element-ui'
 import '@/assets/element-ui/index.css'
 import store from './store/index.js'
 import '@/assets/css/global.css'
+// vue-awesome-swiper插件全局引入 -- 开始
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'    //在全局没引入，这里记得要！
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+// vue-awesome-swiper插件全局引入 -- 结束
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
