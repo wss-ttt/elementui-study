2702fe9efa0654988c2c5c7056499b1e8d2da39e
wangshuisheng <602466011@qq.com>
2020-05-11 21:56:24 +0800

    添加:非父子组件之间传递数据

---
+++ src/views/test5/index3.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="wrapper">
+       <v-a></v-a>
+       <hr>
+       <v-b></v-b>
+   </div>
+</template>
+
+<script>
+import VA from './components/A.vue'
+import VB from './components/B.vue'
+  export default {
+    components: {
+        VA,
+        VB
+    },
+    props: {},
+    data() {
+      return {
+          
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filter: {}
+  }
+</script>
+<style scoped>
+</style>
