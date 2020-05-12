6c730b2965ae612d8e8c46e764f7bd547d33f9d3
王水胜 <602466011@qq.com>
2020-05-12 13:45:11 +0800

    添加: drawer自行封装

---
+++ src/views/test5/components/F.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wt-drawer" v-show="drawer">
+    <div class="mask"></div>
+    <div class="content">aaaa</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      drawer: false
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filter: {}
+}
+</script>
+<style scoped lang="scss">
+.wt-drawer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+  mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.3);
+  }
+}
+</style>
+ 
