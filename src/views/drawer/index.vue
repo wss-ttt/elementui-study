598c9cc97758eea0e7bc4c2ddd3bce0f1dd0e6dc
wangshuisheng <602466011@qq.com>
2020-05-13 21:40:56 +0800

    添加使用drawer组件

---
+++ src/views/drawer/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <button type="button" @click="drawer = true">显示</button>
+    <v-drawer :visible.sync="drawer">
+      <div>
+        好后学习
+      </div>
+    </v-drawer>
+  </div>
+</template>
+
+<script>
+  import VDrawer from './components/Drawer'
+  export default {
+    components: { 
+      VDrawer
+    },
+    props: {},
+    data() {
+      return {
+        drawer: false
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      hide(data) {
+        this.drawer = data
+      }
+    },
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
