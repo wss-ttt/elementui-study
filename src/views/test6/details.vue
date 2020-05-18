3f71459f3b568b8e94d3327db30b70d263913725
wangshuisheng <602466011@qq.com>
2020-05-18 22:49:45 +0800

    更新:路由参数变化 监听

---
+++ src/views/test6/details.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <h2>详情信息</h2>
+    {{ $route.params }}
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {},
+    watch: {
+      $route: 'routeHandle'
+    },
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      routeHandle() {
+        console.log('路由发生变化了')
+      }
+    },
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
---
+++ src/views/test6/list.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <h2>列表</h2>
+    <ul>
+      <li v-for="i in 5" :key="i" @click="toDetails(i)"> {{ i }} </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      toDetails(id) {
+        this.$router.push({
+          name: 'test6-details',
+          params: {
+            id: id
+          }
+        })
+      }
+    },
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
