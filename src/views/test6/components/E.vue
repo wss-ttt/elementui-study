8f3e5031c7b4ea35c3217f4a2328c2ef454df0b2
wangshuisheng <602466011@qq.com>
2020-05-20 20:50:15 +0800

    更新: 父组件钩子函数中存在异步操作子组件获取不到数据问题

---
+++ src/views/test6/components/E.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    我是子路由
+    <el-select v-model="stuId" placeholder="请选择">
+      <el-option v-for="item in studentList" :key="item.id" :label="item.name" :value="item.id">
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        stuId: '',
+      }
+    },
+    computed: {
+      studentList() {
+        return this.$store.state.common.studentList
+      }
+    },
+    watch: {},
+    created() {},
+    mounted() {
+      console.log('我是子组件中的mounted')
+      this.studentList.length && (this.stuId = this.studentList[0]['id'])
+    },
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
---
+++ src/views/test6/index5.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <div class="box">
+      父组件
+      <button type="button" @click="toUrl">跳转路由</button>
+    </div>
+    <h2>内容显示区域</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+    },
+    watch: {},
+    created() {
+      this.getData()
+    },
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      getData() {
+        setTimeout(() => {
+          let data = []
+          for (let i = 0; i < 5; i++) {
+            data.push({
+              id: i,
+              name: '张三' + i
+            })
+          }
+          console.log('父组件中异步获取数据')
+          this.$store.commit('common/updateStudentList', data)
+        }, 0)
+      },
+      toUrl() {
+        this.$router.push({
+          name: 'index5-e'
+        })
+      }
+    },
+    filter: {}
+  }
+
+</script>
+<style scoped lang="scss">
+  .box {
+    height: 200px;
+    border: 1px solid #ccc;
+  }
+
+</style>
