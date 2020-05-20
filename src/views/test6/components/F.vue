964b6a5000c8ff567806e9b6dad109a6b7d3d6e6
wangshuisheng <602466011@qq.com>
2020-05-20 21:22:58 +0800

    更新:父组件钩子函数中存在异步操作保存数据子组件是获取不到该数据的

---
+++ src/views/test6/components/F.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    props: {},
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {
+        console.log('子组件的mounted')
+    },
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
---
+++ src/views/test6/index6.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+      <v-f></v-f>
+  </div>
+</template>
+
+<script>
+  import VF from './components/F'
+  export default {
+    components: {
+      VF
+    },
+    props: {},
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {
+      console.log('父组件中的created')
+      // 1.父组件中模拟异步操作
+      setTimeout(() => {
+        console.log('父组件中模拟异步操作')
+      }, 0)
+    },
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
