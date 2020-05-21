60931d9e589f11e3e243a312ea35d080356a4d0d
wangshuisheng <602466011@qq.com>
2020-05-21 22:43:30 +0800

    更新 : store中存储数组或对象类型的数据引起报错的原因分析

---
+++ src/views/test6/index7.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <button type="button" @click="init">初始数据</button>
+    <button type="button" @click="add">添加数据</button>
+    <ul>
+      <li v-for="item in studentList" :key="item.name">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        list: [{
+          name: '张三',
+          age: 18
+        }, {
+          name: '李四',
+          age: 20
+        }]
+      }
+    },
+    computed: {
+      studentList() {
+        return this.$store.state.common.studentList
+      }
+    },
+    watch: {},
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      init() {
+        this.$store.commit('common/updateStudentList', this.list)
+        console.log(this.list === this.$store.state.common.studentList)// true
+      },
+      add() {
+        this.list.push({
+          name: '乔峰',
+          age: 28
+        })
+        /* this.list = [{
+            name: '乔峰',
+            age: 28
+        }] */
+        // console.log(this.$store.state.common.studentList)
+      }
+    },
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
