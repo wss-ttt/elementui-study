82e728b17f3119263c838a76622ce2f9c652b956
王水胜 <602466011@qq.com>
2020-05-25 15:19:55 +0800

    格式化

--- src/views/test7/index.vue
+++ src/views/test7/index.vue
@@ -1,39 +1,39 @@
 <template>
-    <div class="wrapper">
-       <router-link :to="{name: 'a'}">a</router-link>
-       <router-link :to="{name: 'b'}">b</router-link>
-       <router-link :to="{name: 'c'}">c</router-link>
-       <hr>
-       <keep-alive :include="cache">
-        <router-view></router-view>
-       </keep-alive>
-   </div>
+  <div class="wrapper">
+    <router-link :to="{name: 'a'}">a</router-link>
+    <router-link :to="{name: 'b'}">b</router-link>
+    <router-link :to="{name: 'c'}">c</router-link>
+    <hr />
+    <keep-alive :include="cache">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
-  export default {
-    components: {},
-    props: {},
-    data() {
-      return {
-          cache: ['A', 'B']
-      }
-    },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {},
-    activated() {},
-    deactivated() {},
-    updated() {},
-    destroyed() {},
-    methods: {},
-    filter: {}
-  }
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      cache: ['A', 'B']
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {},
+  filter: {}
+}
 </script>
 <style scoped>
-a{
-    font-size: 30px;
+a {
+  font-size: 30px;
 }
 </style>
  
