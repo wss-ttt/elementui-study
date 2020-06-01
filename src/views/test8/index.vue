4b998988fd1f7d880a18174aaabf99b146865a79
wangshuisheng <602466011@qq.com>
2020-06-01 22:45:13 +0800

    更新: index index2

---
+++ src/views/test8/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <router-link :to="{name: 'AA'}">A</router-link>
+    <router-link :to="{name: 'BB'}">B</router-link>
+    <router-link :to="{name: 'CC'}">C</router-link>
+    <hr>
+    <h2>子路由显示的区域</h2>
+    当前路由信息: {{ $route.meta }}
+    <!-- <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view> -->
+
+
+    <keep-alive v-if="$route.meta.keepAlive">
+      <router-view></router-view>
+    </keep-alive>
+    <router-view v-else></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    watch: {
+      $route(newVal, oldVal) {
+        console.log(newVal, oldVal)
+      }
+    },
+    created() {},
+    mounted() {},
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {},
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
---
+++ src/views/test8/index2.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <d-d v-if="list.length" :list="list"></d-d>
+  </div>
+</template>
+
+<script>
+  import DD from './components/DD'
+  export default {
+    components: {
+      DD
+    },
+    props: {},
+    data() {
+      return {
+        list: []
+      }
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {
+      this.getData()
+    },
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      getData() {
+        this.$ajax({
+          url: '/api/list',
+          method: 'post'
+        }).then(({data}) => {
+          console.log('data', data)
+          this.list = data.data
+        })
+      }
+    },
+    filter: {}
+  }
+
+</script>
+<style scoped>
+</style>
