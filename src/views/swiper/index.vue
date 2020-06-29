69f2b3e0bf1942fa68868e950092e3df6c2d2b89
wangshuisheng <602466011@qq.com>
2020-06-29 22:22:36 +0800

    更新: swiper实例被v-if销毁引发的问题

--- src/views/swiper/index.vue
+++ src/views/swiper/index.vue
@@ -1,71 +1,88 @@
 <template>
   <div class="wrapper">
-    <swiper ref="mySwiper" v-if="visible" :options="swiperOption" class="box">
-      <swiper-slide class="item" style="background: red;">I'm Slide 1</swiper-slide>
-      <swiper-slide class="item" style="background: green;">I'm Slide 2</swiper-slide>
-      <swiper-slide class="item" style="background: yellow;">I'm Slide 3</swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
+    <swiper ref="mySwiper" v-if="list.length" :options="swiperOption" @mouseenter.native="on_enter"
+      @mouseleave.native="on_leave" class="box">
+      <swiper-slide v-for="i in list" :key="i" class="item">{{ i }}</swiper-slide>
     </swiper>
-    <button type="button" @click="visible = !visible">显示/隐藏</button>
-    <button type="button" @click="show">显示</button>
-    <button type="button" @click="stop">stop</button>
-    <button type="button" @click="start">start</button>
+    <button type="button" @click="getData">异步请求(有数据)</button>
+    <button type="button" @click="getData2">异步请求(没有数据)</button>
+    <button type="button" @click="showSwiper">获取swiper实例</button>
   </div>
 </template>
 
 <script>
-export default {
-  components: {},
-  props: {},
-  data() {
-    return {
-      visible: true,
-      swiperOption: {
-        direction: 'vertical',
-        autoplay: true,
-        loop: true
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        list: [],
+        visible: true,
+        swiperOption: {
+          direction: 'vertical',
+          slidesPerView: '5', // 容器中显示几个
+          autoplay: true,
+          loop: true
+        }
       }
-    }
-  },
-  computed: {
-    mySwiper() {
-      // return this.$refs['mySwiper'].$swiper
-      return this.$refs['mySwiper'].swiperInstance
-    }
-  },
-  watch: {},
-  created() {},
-  mounted() {},
-  activated() {},
-  deactivated() {},
-  updated() {},
-  destroyed() {},
-  methods: {
-    show() {
-      console.log(this.mySwiper)
     },
-    stop() {
-      const res = this.mySwiper.autoplay.stop()
-      console.log(res)
+    computed: {
+      mySwiper() {
+        return this.$refs['mySwiper'].$swiper
+      }
     },
-    start() {
-      const res = this.mySwiper.autoplay.start()
-      console.log(res)
-    }
-  },
-  filter: {}
-}
+    watch: {},
+    created() {},
+    mounted() {
+      // 请求数据
+      this.getData()
+    },
+    activated() {},
+    deactivated() {},
+    updated() {},
+    destroyed() {},
+    methods: {
+      on_enter() {
+        // this.mySwiper.autoplay.stop()
+        this.$refs['mySwiper'].$swiper.autoplay.stop()
+      },
+      on_leave() {
+        // this.mySwiper.autoplay.start()
+        this.$refs['mySwiper'].$swiper.autoplay.start()
+      },
+      getData() {
+        setTimeout(() => {
+          for (let i = 1; i <= 10; i++) {
+            this.list.push(i)
+          }
+        }, 1000)
+      },
+      getData2() {
+        setTimeout(() => {
+          this.list = []
+        }, 1000)
+      },
+      showSwiper() {
+        console.log(this.mySwiper)
+        console.log(this.$refs['mySwiper'])
+        console.log(this.$refs['mySwiper'].$swiper)
+      }
+    },
+    filter: {}
+  }
+
 </script>
 <style scoped lang="scss">
-.box {
-  width: 400px;
-  height: 400px;
-  background: #fb3;
-  .item{
+  .box {
+    width: 400px;
+    height: 400px;
+    background: #fb3;
+
+    .item {
       font-size: 30px;
       text-align: center;
       color: #fff;
+    }
   }
-}
+
 </style>
- 
