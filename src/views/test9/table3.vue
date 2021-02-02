5675a0b86cce075f8138d54e490eac50d573a963
王水胜 <602466011@qq.com>
2021-02-02 11:56:57 +0800

    添加: test9>table3.vue v-if 实现列的显示和隐藏

---
+++ src/views/test9/table3.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div>
+      <label>请选择表格需要展示的列：</label>
+      <el-checkbox-group v-model="checkedTableColumns">
+        <el-checkbox
+          v-for="column in ctrlArr"
+          :key="column.prop"
+          :label="column.prop"
+        >{{ column.label }}</el-checkbox>
+      </el-checkbox-group>
+    </div>
+    <el-table ref="singleTable" :data="tableData" border>
+      <el-table-column property="date" label="日期" v-if="ctrlArr[0].show"></el-table-column>
+      <el-table-column property="name" label="姓名" v-if="ctrlArr[1].show"></el-table-column>
+      <el-table-column property="address" label="地址" v-if="ctrlArr[2].show"></el-table-column>
+    </el-table>
+
+    <!-- <el-checkbox-group v-model="checkList">
+      <el-checkbox label="复选框 A"></el-checkbox>
+      <el-checkbox label="复选框 B"></el-checkbox>
+      <el-checkbox label="复选框 C"></el-checkbox>
+      <el-checkbox label="禁用" disabled></el-checkbox>
+      <el-checkbox label="选中且禁用" disabled></el-checkbox>
+    </el-checkbox-group>-->
+    <button type="button" @click="test">确定</button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      checkList: ['选中且禁用', '复选框 A'],
+      ctrlArr: [
+        {
+          prop: 'date',
+          show: true,
+          label: '日期'
+        },
+        {
+          prop: 'name',
+          show: true,
+          label: '姓名'
+        },
+        {
+          prop: 'address',
+          show: true,
+          label: '地址'
+        }
+      ],
+      // 数据源
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }
+      ]
+    }
+  },
+  computed: {
+    bindTableColumns() {
+      // 过滤table需要显示的列(字段)
+      return this.ctrlArr.filter(column => column.show)
+    },
+    /* 这里使用了getter和setter，这样写的好处不用自己手动监听复选框的选中事件 */
+    checkedTableColumns: {
+      get() {
+        return this.bindTableColumns.map(column => column.prop) // 这里只需要返回prop值
+        // return ['date', 'name'] // 只会有日期 姓名会被选中
+      },
+      set(checked) {
+        // 设置表格列的显示与隐藏
+        this.ctrlArr.forEach((column, index) => {
+          // 如果选中，则设置列显示
+          if (checked.includes(column.prop)) {
+            column.show = true
+          } else {
+            // 如果未选中，则设置列隐藏
+            column.show = false
+          }
+        })
+      }
+    }
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    handleChecked(val) {},
+    test() {
+      console.log('aa', this.checkList)
+    }
+  },
+  filters: {}
+}
+</script>
+<style scoped>
+.select-column {
+  float: right;
+}
+.el-checkbox-group {
+  /* display: flex; */
+  /* flex-direction: column; */
+}
+</style>
