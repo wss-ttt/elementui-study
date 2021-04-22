ab1e334d603a40a3f624398c181d7281fa9c38e5
王水胜 <602466011@qq.com>
2021-04-22 15:37:43 +0800

    更新： global.js

--- src/global.js
+++ src/global.js
@@ -1,20 +1,10 @@
-import {test} from '@/utils/index'
-let startTime = new Date()
-test()
-/* module.exports = {
-    name: '乔峰',
-    say() {
-        console.log('好好学习')
-    },
-    startTime
-} */
-
-let name = '乔峰'
-function say(){
-    console.log('好好学习')
-}
+// 全局配置文件 -> 存储全局业务数据
 export default {
-    name,
-    say,
-    startTime
+  warnValue: 0.1776, // 告警的阈值-比差
+  abnormalValue: 0.32, // 异常的阈值-比差
+  monitiorType: ['正常', '注意', '异常'],
+  warnErrorValue: 7, // 告警的阈值-角差
+  abnormalErrorValue: 18, // 异常的阈值-角差
+  monitorColor: ['#03C1FF', '#00FF00', '#FFA500'] // 对应关系: 正常 注意 异常
+  // 之前的颜色: "#04C1FC", "#FFD738", "#FF2419"
 }
