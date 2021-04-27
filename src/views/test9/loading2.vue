3286b3433988feff92d95ab5865ecfa1284995b5
王水胜 <602466011@qq.com>
2021-04-27 15:16:39 +0800

    更新: Line Scale Party

--- src/views/test9/loading2.vue
+++ src/views/test9/loading2.vue
@@ -15,8 +15,19 @@
         </div>
         <div class="item-title"><a href="#">Line Scale<span>→</span></a></div>
       </el-col>
-      <el-col :span="6">
-        aaa
+      <el-col :span="6" class="item">
+        <div class="item-inner">
+          <div class="item-loader-container">
+            <div class="la-line-scale-party la-2x">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div class="item-title"><a href="#">Line Scale Party<span>→</span></a></div>
       </el-col>
       <el-col :span="6">
         aaa
@@ -68,41 +79,6 @@
     color: rgba(255, 255, 255, .7);
   }
 
-  .la-line-scale {
-    @include center();
-    border: 1px solid red;
-    color: #fff;
-    width: 40px;
-    height: 32px;
-
-    &.la-2x {
-      width: 80px;
-      height: 64px;
-
-      >div {
-        width: 8px;
-        height: 64px;
-        margin: 4px;
-      }
-    }
-
-    >div {
-      width: 4px;
-      height: 32px;
-      margin: 2px;
-      background-color: #fff;
-      animation: line-scale 1.2s infinite ease;
-    }
-
-
-    >div {
-      @for $i from 1 through 5 {
-        &:nth-child(#{$i}) {
-          animation-delay: -1.2s + $i * 0.1; // 延迟动画时间
-        }
-      }
-    }
-  }
 
   .item {
     height: 200px;
@@ -111,7 +87,6 @@
     transition: all 250ms linear;
 
     .item-inner {
-      border: 1px solid red;
       position: relative;
       display: flex;
       width: 100%;
@@ -168,6 +143,89 @@
     }
   }
 
+  .la-line-scale {
+    @include center();
+    color: #fff;
+    width: 40px;
+    height: 32px;
+
+    &.la-2x {
+      width: 80px;
+      height: 64px;
+
+      >div {
+        width: 8px;
+        height: 64px;
+        margin: 4px;
+      }
+    }
+
+    >div {
+      width: 4px;
+      height: 32px;
+      margin: 2px;
+      background-color: #fff;
+      animation: line-scale 1.2s infinite ease;
+
+      @for $i from 1 through 5 {
+        &:nth-child(#{$i}) {
+          animation-delay: -1.2s + $i * 0.1; // 延迟动画时间
+        }
+      }
+    }
+  }
+
+  .la-line-scale-party {
+    @include center();
+    color: #fff;
+    width: 40px;
+    height: 32px;
+
+    &.la-2x {
+      width: 80px;
+      height: 64px;
+
+      >div {
+        width: 8px;
+        height: 64px;
+        margin: 4px;
+      }
+    }
+
+    >div {
+      width: 4px;
+      height: 32px;
+      margin: 2px;
+      background-color: #fff;
+      animation: line-scale-party 1.2s infinite ease;
+
+      &:nth-child(1) {
+        animation-duration: .43s;
+        animation-delay: -.23s;
+      }
+
+      &:nth-child(2) {
+        animation-duration: .62s;
+        animation-delay: -.32s;
+      }
+
+      &:nth-child(3) {
+        animation-duration: .43s;
+        animation-delay: -.44s;
+      }
+
+      &:nth-child(4) {
+        animation-duration: .8s;
+        animation-delay: -.31s;
+      }
+
+      &:nth-child(5) {
+        animation-duration: .74s;
+        animation-delay: -.24s;
+      }
+    }
+  }
+
   @keyframes line-scale {
 
     0%,
@@ -177,6 +235,21 @@
     }
 
     20% {
+      transform: scaleY(1);
+    }
+  }
+
+  @keyframes line-scale-party {
+
+    0% {
+      transform: scaleY(1);
+    }
+
+    50% {
+      transform: scaleY(0.3);
+    }
+
+    100% {
       transform: scaleY(1);
     }
   }
