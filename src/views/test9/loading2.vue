c9bc8616fdd408ede9b9c02fdbee5b2354c01388
王水胜 <602466011@qq.com>
2021-04-27 17:19:46 +0800

    更新：Ball Triangle Path

--- src/views/test9/loading2.vue
+++ src/views/test9/loading2.vue
@@ -41,6 +41,17 @@
         </div>
         <div class="item-title"><a href="#">Ball Newton Cradle<span>→</span></a></div>
       </el-col>
+      <el-col :span="6" class="item">
+        <div class="item-inner">
+          <div class="item-loader-container">
+            <div class="la-ball-triangle-path la-2x">
+              <div v-for="i in 3" :key="'ball-triangle-path' + i"></div>
+            </div>
+            <div class="item-title"><a href="#">Ball Triangle Path<span>→</span></a>
+            </div>
+          </div>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -327,6 +338,44 @@
     }
   }
 
+  .la-ball-triangle-path {
+    width: 32px;
+    height: 32px;
+    position: relative;
+
+    &.la-2x {
+      width: 64px;
+      height: 64px;
+
+      >div {
+        width: 20px;
+        height: 20px;
+      }
+    }
+
+    >div {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 10px;
+      height: 10px;
+      background-color: #fff;
+      border-radius: 50%;
+
+      &:nth-child(1) {
+        animation: ball-triangle-path-ball-one 2s 0s ease-in-out infinite;
+      }
+
+      &:nth-child(2) {
+        animation: ball-triangle-path-ball-two 2s 0s ease-in-out infinite;
+      }
+
+      &:nth-child(3) {
+        animation: ball-triangle-path-ball-three 2s 0s ease-in-out infinite;
+      }
+    }
+  }
+
 
 
   @keyframes line-scale {
@@ -397,6 +446,105 @@
 
     100% {
       transform: translateX(0%);
+    }
+  }
+
+  @keyframes ball-triangle-path-ball-one {
+    0% {
+      transform: translate(0, 220%);
+    }
+
+    17% {
+      opacity: .25;
+    }
+
+    33% {
+      opacity: 1;
+      transform: translate(110%, 0);
+    }
+
+    50% {
+      opacity: .25;
+    }
+
+    66% {
+      opacity: 1;
+      transform: translate(220%, 220%);
+    }
+
+    83% {
+      opacity: .25;
+    }
+
+    100% {
+      opacity: 1;
+      transform: translate(0, 220%);
+    }
+  }
+
+  @keyframes ball-triangle-path-ball-two {
+    0% {
+      transform: translate(110%, 0);
+    }
+
+    17% {
+      opacity: .25;
+    }
+
+    33% {
+      opacity: 1;
+      transform: translate(220%, 220%);
+    }
+
+    50% {
+      opacity: .25;
+    }
+
+    66% {
+      opacity: 1;
+      transform: translate(0, 220%);
+    }
+
+    83% {
+      opacity: .25;
+    }
+
+    100% {
+      opacity: 1;
+      transform: translate(110%, 0);
+    }
+  }
+
+  @keyframes ball-triangle-path-ball-three {
+    0% {
+      transform: translate(220%, 220%);
+    }
+
+    17% {
+      opacity: .25;
+    }
+
+    33% {
+      opacity: 1;
+      transform: translate(0, 220%);
+    }
+
+    50% {
+      opacity: .25;
+    }
+
+    66% {
+      opacity: 1;
+      transform: translate(110%, 0);
+    }
+
+    83% {
+      opacity: .25;
+    }
+
+    100% {
+      opacity: 1;
+      transform: translate(220%, 220%);
     }
   }
 
