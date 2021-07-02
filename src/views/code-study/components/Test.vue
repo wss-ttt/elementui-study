511a2d4789bfa77baf7f3f0e6bad0bfd4067ca65
王水胜 <602466011@qq.com>
2021-07-02 14:50:19 +0800

    添加: 测试组件

---
+++ src/views/code-study/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="test" @click="handleClick">{{ value }}</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      currentValue: this.value
+    }
+  },
+  methods: {
+    handleClick() {
+      this.currentValue++
+      this.$emit('input', this.currentValue)
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  activated() {},
+  deactivated() {},
+  updated() {},
+  destroyed() {},
+  filters: {}
+}
+</script>
+<style scoped lang="scss">
+.test {
+  border: 1px solid red;
+}
+</style>
